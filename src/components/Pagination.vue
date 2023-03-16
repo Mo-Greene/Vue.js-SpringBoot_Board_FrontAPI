67f4ad5985e1638ebf3f03ee90558f3be854497b
Mo-Greene <13blueboy13@daum.net>
2023-03-16 22:22:57 +0900

    feat: 페이지네이션 시작

---
+++ src/components/Pagination.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="d-flex justify-content-center">
+    <ul class="pagination flex-wrap">
+      <li :key="i" v-for="(page,i) in pagination">
+        <a class="page-item">{{ page.page }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import {reactive} from "vue";
+import axios from "axios";
+
+export default {
+  name: "Pagination",
+  setup() {
+    const state = reactive({
+      pagination: []
+    })
+
+    axios.get("http://localhost:8080/boards").then(({data}) => {
+      state.pagination = data.resultData.page
+      console.log(state.pagination)
+    })
+
+    return state
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,13 +1,15 @@
 <template>
   <Table/>
+  <Pagination/>
 </template>
 
 <script>
 import Table from "@/components/Table";
+import Pagination from "@/components/Pagination";
 
 export default {
   name: "Home",
-  components: {Table}
+  components: {Pagination, Table}
 
 }
 </script>
